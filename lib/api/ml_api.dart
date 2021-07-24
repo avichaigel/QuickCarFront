f786cf3295427bb3212136255f03f624d66a13e8
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-07-24 22:07:59 +0300

    new url ml

--- lib/api/ml_api.dart
+++ lib/api/ml_api.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:quick_car/data_class/car_detection_response.dart';
-import 'package:async/async.dart';
 
 class MLApi {
   Future<CarDetectionResponse>isCar(File image) async {
     print("Check if image is car");
-    var uri = Uri.parse("http://84434df4-4d6e-4025-9374-e00a4cb25e26.southcentralus.azurecontainer.io/score");
+    var uri = Uri.parse('http://1a29cae6-58b5-4461-bec2-8bd5ab7728a6.centralus.azurecontainer.io/score');
     var bytes = await image.readAsBytes();
     var response = await http.post(uri, body: bytes);
     if (response.statusCode == 200) {
