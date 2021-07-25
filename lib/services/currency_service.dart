e3e309e25d238225dec7f51e378f47366ced3bf9
avichai geldzaher <avichaig@gmail.com>
2021-07-25 23:18:26 +0300

    fixed currency api

--- lib/services/currency_service.dart
+++ lib/services/currency_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:currency_picker/currency_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -20,14 +22,14 @@ class MyCurrencyService extends ChangeNotifier {
     }
     var client = http.Client();
     try {
-      var url =
-          "https://currency-exchange.p.rapidapi.com/exchange?to=$currencyCode&from=USD&q=1.0";
+      var url = "https://fixer-fixer-currency-v1.p.rapidapi.com/convert?amount=1&to=$currencyCode&from=USD";
       var response = await client.get(Uri.parse(url), headers: {
         'x-rapidapi-key': 'cec120a60dmsha3ae2f4ff86c4dfp10736djsn0a7c7d7c5056',
-        'x-rapidapi-host': 'currency-exchange.p.rapidapi.com'
+        'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com'
       }).timeout(Duration(seconds: 3));
       if (response.statusCode == 200) {
-        var value = response.body;
+        var map = jsonDecode(response.body);
+        var value = map['result'];
         currentCurrencyRate = double.parse(value);
         if (currentCurrencyRate == 0) {
           return goBackToDollars(false);
